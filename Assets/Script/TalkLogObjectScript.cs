988313ae1e677832ef6e16f4420ca25029e49f90
song7446 <97102094+song7446@users.noreply.github.com>
2024-08-27 00:18:01 +0900

    20240826 마무리

--- Assets/Script/TalkLogObjectScript.cs
+++ Assets/Script/TalkLogObjectScript.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TalkLogObjectScript : MonoBehaviour
 {
     public static TalkLogObjectScript instance;
-    
+
     private void Awake()
     {
         if (TalkLogObjectScript.instance == null)
@@ -18,4 +18,20 @@ public class TalkLogObjectScript : MonoBehaviour
 
     public Text Name;
     public Text Text;
+    public Button useTextBtn;
+
+    private void Start()
+    {
+        useTextBtn.onClick.AddListener(useTextBtn_onClick);
+    }
+
+    public void useTextBtn_onClick()
+    {
+        Debug.Log("텍스트 버튼 클릭");
+        Vector2 mousePosition = Input.mousePosition;
+        TextUseButtonScript.instance.textUseBtn.transform.position = mousePosition;
+        TextUseButtonScript.instance.textUseBtn.gameObject.SetActive(true);
+        Name.color = Color.red;
+        Text.color = Color.red;
+    }
 }
---
+++ Assets/Script/TextUseButtonScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUseButtonScript : MonoBehaviour
+{
+    public static TextUseButtonScript instance;
+
+    private void Awake()
+    {
+        if (TextUseButtonScript.instance == null)
+        {
+            TextUseButtonScript.instance = this;
+
+        }
+    }
+
+    public Button textUseBtn; 
+
+    void Start()
+    {
+        textUseBtn.onClick.AddListener(textUseBtn_onClick);
+        textUseBtn.gameObject.SetActive(false);
+    }
+
+    void textUseBtn_onClick()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
